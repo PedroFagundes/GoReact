c5d4ffdc61e2288ceebecba3382719c1f0b9ed56
Pedro Henrique Fagundes Botelho <pedrofagundesb@gmail.com>
2018-07-22 16:48:13 -0300

    First commit

---
+++ module01/src/index.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+
+import Button from './components/Button';
+
+import './style.scss';
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h1>
+          Hello Rocketseat
+        </h1>
+        <h2>
+          {this.state.counter}
+        </h2>
+        <Button onClick={this.handleClick}>
+          Somar
+        </Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
